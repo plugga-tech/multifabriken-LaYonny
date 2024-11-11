fa11cfa0a6f9d8803b760f7cb543bbe3c789bdce
Jonathan Baasner <jonathanbaasner@mac.home>
2024-11-11 19:46:54 +0100

    basic menyval with a if stament and a switch for menyval

--- App.java
+++ App.java
@@ -1,6 +1,48 @@
+import java.util.Scanner;
+
 public class App {
 
+    public boolean run = true;
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    
+
+        //menyval 1-6
+
+        System.out.println("Välj mellan 1-6");
         
+
+        if (scan.hasNextInt()) {
+            int meny = scan.nextInt();
+       
+            switch (meny) {
+                case 1:
+                    System.out.println("Bilar");
+                    break;
+                    case 2:
+                    System.out.println("Godis");
+                    break;
+                    case 3:
+                    System.out.println("Rör");
+                    break;
+                    case 4:
+                    System.out.println("Havremjölk");
+                    break;
+                    case 5:
+                    System.out.println("Lista med alla beställda produkter");
+                    break;
+                    case 6:
+                    System.out.println("Avsluta programmet");
+                    scan.close();
+                    break;
+            
+                default:
+                System.out.println("Felmeddelande, siffror stämmer ej med 1-6)");
+                    break;
+            }
+        }
+        else{
+            System.out.println("Felmeddelande för felinput (bokstäver etc)");
+        }
     }
 }
