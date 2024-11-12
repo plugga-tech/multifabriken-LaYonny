e108f7bf33a68441bf8ed6929cbc33a075dd2bce
Jonathan Baasner <jonathanbaasner@mac.home>
2024-11-12 16:25:46 +0100

    adding do while loop to menyval, also bug fix for wrong input

--- App.java
+++ App.java
@@ -1,48 +1,52 @@
 import java.util.Scanner;
 
 public class App {
+    public static boolean run = true;
+ 
 
-    public boolean run = true;
     public static void main(String[] args) {
+        
         Scanner scan = new Scanner(System.in);
     
+        do {
 
         //menyval 1-6
+        System.out.println("Välj mellan 1-6");   
 
-        System.out.println("Välj mellan 1-6");
+            if (scan.hasNextInt()) {
+                int meny = scan.nextInt();
         
-
-        if (scan.hasNextInt()) {
-            int meny = scan.nextInt();
-       
-            switch (meny) {
-                case 1:
-                    System.out.println("Bilar");
-                    break;
-                    case 2:
-                    System.out.println("Godis");
-                    break;
-                    case 3:
-                    System.out.println("Rör");
-                    break;
-                    case 4:
-                    System.out.println("Havremjölk");
-                    break;
-                    case 5:
-                    System.out.println("Lista med alla beställda produkter");
-                    break;
-                    case 6:
-                    System.out.println("Avsluta programmet");
-                    scan.close();
-                    break;
-            
-                default:
-                System.out.println("Felmeddelande, siffror stämmer ej med 1-6)");
-                    break;
+                switch (meny) {
+                    case 1:
+                        System.out.println("Bilar");
+                        break;
+                        case 2:
+                        System.out.println("Godis");
+                        break;
+                        case 3:
+                        System.out.println("Rör");
+                        break;
+                        case 4:
+                        System.out.println("Havremjölk");
+                        break;
+                        case 5:
+                        System.out.println("Lista med alla beställda produkter");
+                        break;
+                        case 6:
+                        System.out.println("Avsluta programmet");
+                        run = false;
+                        scan.close();
+                        break;
+                
+                    default:
+                    System.out.println("Felmeddelande, siffror stämmer ej med 1-6)");
+                        break;
+                }
             }
-        }
-        else{
-            System.out.println("Felmeddelande för felinput (bokstäver etc)");
-        }
+            else{
+                System.out.println("Felmeddelande för felinput (bokstäver etc)");
+                scan.next();
+            }
+        } while (run);
     }
 }
