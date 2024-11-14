f3a4d680a99a5ca116a3d5be9ea699ff577d46a5
Jonathan Baasner <jonathanbaasner@mac.home>
2024-11-14 16:49:35 +0100

    i have no ide whats going on =D

--- products/candy/Candy.java
+++ products/candy/Candy.java
@@ -1,10 +1,10 @@
 package products.candy;
 
-public class CandyProduct {
+public class Candy {
     private String taste;
     private int numberOfCandy;
     
-    public CandyProduct(String taste, int numberOfCandy){
+    public Candy(String taste, int numberOfCandy){
         this.taste = taste;
         this.numberOfCandy = numberOfCandy;
 
