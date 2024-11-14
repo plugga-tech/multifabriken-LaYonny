a5693928f75ca6ff2bfd0c2346e6c367006c8e7e
Jonathan Baasner <jonathanbaasner@mac.home>
2024-11-14 16:39:21 +0100

    file name change for products

--- products/candy/Candy.java
+++ products/candy/Candy.java
@@ -1,26 +1,29 @@
 package products.candy;
 
-import java.util.Scanner;
+public class CandyProduct {
+    private String taste;
+    private int numberOfCandy;
+    
+    public CandyProduct(String taste, int numberOfCandy){
+        this.taste = taste;
+        this.numberOfCandy = numberOfCandy;
 
-public class Candy extends CandyProduct{
-    Scanner scan = new Scanner(System.in);
-
-    public Candy(String taste, int numberOfCandy){
-
-        super(taste,numberOfCandy);
     }
-    //Basic code to test
-    public void addCandyContent(String taste, int numberOfCandy){
-        
-        System.out.println("Välj smak på godiset " +taste);
-        taste = scan.nextLine();
 
-        System.out.println("Antal " + numberOfCandy);
-        numberOfCandy = scan.nextInt();
-        scan.nextLine();
+    public String getTaste() {
+        return taste;
+    }
 
-        System.out.println("Smaken är " +taste + " "+ "Antal godisar: " + numberOfCandy);
+    public void setTaste(String taste) {
+        this.taste = taste;
+    }
 
+    public int getNumberOfCandy() {
+        return numberOfCandy;
+    }
+
+    public void setNumberOfCandy(int numberOfCandy) {
+        this.numberOfCandy = numberOfCandy;
     }
     
 }
--- products/oakmilk/Oakmilk.java
+++ products/oakmilk/Oakmilk.java
@@ -1,24 +1,34 @@
 package products.oakmilk;
 
-import java.util.Scanner;
+public class OakProduct {
+    private double  fatContent;
+    private double volume;
+    
 
-public class  Oakmilk extends OakProduct {
-    Scanner scan = new Scanner(System.in);
-
-    public Oakmilk(int fatContent, int vloume){
-      
-        super(fatContent,vloume);  
-
+    public OakProduct(double fatContent, double volume){
+        this.fatContent = fatContent;
+        this.volume = volume;
     }
 
-    public void addOakMilkContent(double fatContent, double vloume){
-        
-        System.out.println("Skriv in fetthalt");
-        fatContent = scan.nextDouble();
 
-        System.out.println("Skriv in Litermängd");
-        vloume = scan.nextDouble();
-
-        System.out.println("fetthalt är " +fatContent + " " + "Litermängd " +vloume);
+    public double getFatContent() {
+        return fatContent;
     }
+
+
+    public void setFatContent(double fatContent) {
+        this.fatContent = fatContent;
+    }
+
+
+    public double getVolume() {
+        return volume;
+    }
+
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    
 }
